e22233d9959c2e4b3a851d5b6346406785778053
Pankratov <pnrf@yandex.ru>
2025-04-10 18:05:38 +0300

    build: add portfolio & blog sections

---
+++ src/components/blog-section/blog-section.jsx
@@ -0,0 +1,18 @@
+import styles from './blog-section.module.css'
+
+export default function BlogSection() {
+  return (
+		<section className={styles['blog-section']}>
+			<h2 className={styles['blog-section__heading']}>Блог</h2>
+			<div className={styles['blog-section__container']}>
+				<div className={styles['blog-section__card']}>
+					<ul className={styles['blog-section__card-list']}>
+						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='#'>Как сделать сайт с документацией (пошаговая инструкция)</a><span className={styles['blog-section__card-list-item-date']}> – Апрель 10, 2025</span></li>
+						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='#'>Зачем техническому писателю сайт-портфолио</a><span className={styles['blog-section__card-list-item-date']}> – Март 14, 2025</span></li>
+					</ul>
+					<a href='/docs'>Перейти в блог →</a>
+				</div>
+			</div>
+		</section>
+  )
+}
