089bf7771c37ba64d65252b1f8d0f5f60ddff2eb
Pankratov <pnrf@yandex.ru>
2025-04-11 01:50:59 +0300

    docs: update content

--- src/components/blog-section/blog-section.jsx
+++ src/components/blog-section/blog-section.jsx
@@ -7,8 +7,8 @@ export default function BlogSection() {
 			<div className={styles['blog-section__container']}>
 				<div className={styles['blog-section__card']}>
 					<ul className={styles['blog-section__card-list']}>
-						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='#'>Как сделать сайт с документацией (пошаговая инструкция)</a><span className={styles['blog-section__card-list-item-date']}> – Апрель 10, 2025</span></li>
-						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='#'>Зачем техническому писателю сайт-портфолио</a><span className={styles['blog-section__card-list-item-date']}> – Март 14, 2025</span></li>
+						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='/blog/kak-sdelat-sayt-s-tekhnicheskoy-dokumentatsiyey'>Как сделать сайт с документацией (пошаговая инструкция)</a><span className={styles['blog-section__card-list-item-date']}> – Апрель 10, 2025</span></li>
+						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='/blog/zachem-tekhpisatelyu-sayt-portfolio'>Зачем техническому писателю сайт-портфолио</a><span className={styles['blog-section__card-list-item-date']}> – Март 14, 2025</span></li>
 					</ul>
 					<a href='/blog'>Перейти в блог →</a>
 				</div>
---
+++ src/content/blog/_meta.js
@@ -0,0 +1,14 @@
+export default {
+	index: {
+		title: 'Содержание',
+		theme: {
+			// breadcrumbs: false,
+			pagination: false
+		}
+	},
+	'---': {
+    type: 'separator'
+  },
+	'kak-sdelat-sayt-s-tekhnicheskoy-dokumentatsiyey': 'Как сделать сайт с документацией',
+	'zachem-tekhpisatelyu-sayt-portfolio': 'Зачем техписателю сайт-портфолио'
+}
