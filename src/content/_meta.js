d7bfe46755bc258b625f00d3234893f191db7f7f
Pankratov <pnrf@yandex.ru>
2025-04-10 14:31:15 +0300

    build: optimize hero-section for screen resolution

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -3,7 +3,13 @@ export default {
 		type: "page"
 	},
 	index: {
-		title: 'Обо мне'
+		title: 'Обо мне',
+		theme: {
+			navbar: false,
+			pagination: false,
+			sidebar: false,
+			toc: false
+		}
 	},
 	projects: {
 		title: 'Проекты'
