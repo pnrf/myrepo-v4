972726845b4ddbbb703f6b1e0387248617416fa4
Pankratov <pnrf@yandex.ru>
2025-04-28 08:43:40 +0300

    docs: update content

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -2,6 +2,7 @@ export default {
 	index: {
 		title: 'Обо мне',
 		type: 'page',
+		display: 'hidden',
 		theme: {
 			layout: 'full',
 			pagination: false,
@@ -10,35 +11,17 @@ export default {
 			sidebar: false
 		}
 	},
+	skills: {
+		title: 'Навыки',
+		type: 'page'
+	},
 	portfolio: {
 		title: 'Портфолио',
 		type: 'page'
 	},
-	skills: {
-		title: 'Навыки',
-		type: 'menu',
-    items: {
-			'index': {
-        title: '→ Умения и навыки',
-        href: '/skills'
-			},
-      'dokumentirovaniye-programmnykh-produktov': {
-        title: '→ Документирование программных продуктов',
-        href: '/skills/dokumentirovaniye-programmnykh-produktov'
-			},
-			'sozdaniye-kontenta': {
-        title: '→ Создание контента для интерфейсов и баз знаний',
-        href: '/skills/sozdaniye-kontenta'
-			},
-      'sostavleniye-yuridicheskikh-dokumentov': {
-        title: '→ Составление юридических документов',
-        href: '/skills/sostavleniye-yuridicheskikh-dokumentov'
-			},
-			'razrabotka-saytov-s-dokumentatsiyey': {
-        title: '→ Вёрстка сайтов с документацией',
-        href: '/skills/razrabotka-saytov-s-dokumentatsiyey'
-			}
-		}
+	'knowledge-base': {
+		title: 'База знаний',
+		type: 'page'
 	},
 	blog: {
 		title: 'Блог',
