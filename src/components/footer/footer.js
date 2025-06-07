13a4ed41a34073dfce7f1bd71b1bb51e554dca4e
Pankratov <pnrf@yandex.ru>
2025-06-07 16:01:23 +0300

    docs: update content

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,8 @@ export default function MyFooter() {
 				<p className={styles['footer__text']}>Myrepo.ru – сайт-портфолио технического писателя</p>
 				<p className={`${styles['footer__text']} ${styles['footer__text-margins']}`}>© 2024-{new Date().getFullYear()}{', Панкратов Ю.А.'} {' Все права защищены'} </p>
 				<p className={styles['footer__text']}>Сайт работает на <a href="https://github.com" target="_blank"> Github</a>, <a href="https://vercel.com" target="_blank">Vercel</a>, <a href="https://react.dev" target="_blank"> React</a>, <a href="https://nextjs.org" target="_blank"> Next.js</a> и <a href="https://nextra.site" target="_blank"> Nextra</a></p>
-				<p className={styles['footer__text']}>Изображения сгенерированы <a href="https://ya.ru/ai/art" target="_blank">нейросетью Яндекса</a></p>
+				<p className={styles['footer__text']}>Изображение на главной странице со <a href="https://storyset.com/illustration/researching/bro#647F94FF&hide=&hide=simple" target="_blank">Storyset by Freepik</a></p>
+				<p className={`${styles['footer__text']} ${styles['footer__text-margins']}`}>Контент составлен с помощью <a href="https://ya.ru/ai/art" target="_blank">YandexART</a> и <a href="https://ya.ru/ai/gpt" target="_blank">YandexGPT</a></p>
 				<p className={styles['footer__text']}>Возрастные ограничения и применимое право:</p>
 				<div className={styles['footer__icons-wrapper']}>
 					<span className={`${styles['footer__icon']} ${styles['footer__icon_highlight']}`}>18+</span>
