8b1c87a6fe5960a93e2877d6af0939d831b12a74
Pankratov <pnrf@yandex.ru>
2025-06-13 23:12:43 +0300

    style: faq section

--- src/components/faq-section/faq-section.jsx
+++ src/components/faq-section/faq-section.jsx
@@ -1,4 +1,6 @@
 import styles from '../blog-section/blog-section.module.css'
+import faqStyles from './faq-section.module.css'
+
 
 export default function FaqSection() {
   return (
@@ -6,11 +8,20 @@ export default function FaqSection() {
 			<h2 className={styles['blog-section__heading']}>FAQ</h2>
 			<div className={styles['blog-section__container']}>
 				<div className={styles['blog-section__card']}>
-					<ul className={styles['blog-section__card-list']}>
-						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='/faq/qqq'>Чем вы можете нам помочь?</a></li>
-						<li className={styles['blog-section__card-list-item']}><a className={styles['blog-section__link']} href='/faq/qqq'>Какой у вас опыт работы в сфере IT?</a></li>
-					</ul>
-					<a href='/faq'>Ответы на все вопросы →</a>
+					<details className={faqStyles['faq-section__details']}>
+						<summary className={faqStyles['faq-section__summary']}>Чем вы можете нам помочь? </summary>
+							<div>- Врач-рентгенолог при проведении инструментального обследования пациента.</div>
+					</details>
+					<details className={faqStyles['faq-section__details']}>
+						<summary className={faqStyles['faq-section__summary']}>Какой у вас опыт работы в сфере IT?</summary>
+							<div>
+								- Врач-рентгенолог при проведении инструментального обследования пациента.  
+								- Врач-рентгенолог при проведении инструментального обследования пациента.  
+								- Врач-рентгенолог при проведении инструментального обследования пациента.  
+								- Врач-рентгенолог при проведении инструментального обследования пациента.  
+							</div>
+					</details>
+					<a href='/faq' className={faqStyles['faq-section__link']}>Ответы на все вопросы →</a>
 				</div>
 			</div>
 		</section>
