88a7347a52ef27fd786eccc44d22dadccdf2b6b6
Pankratov <pnrf@yandex.ru>
2025-06-17 16:10:25 +0300

    docs: update content

--- src/content/knowledge-base/concepts/_meta.js
+++ src/content/knowledge-base/concepts/_meta.js
@@ -2,7 +2,9 @@ export default {
 	'bem': 'БЭМ',
 	'bpmn': 'BPMN',
 	'docs-as-code': 'Docs as Code',
+	para: 'P.A.R.A.',
 	'pixel-perfect': 'Pixel Perfect',
 	'rest-api': 'REST API',
+	'single-source-publishing': 'Single Source Publishing',
 	'uml': 'UML'
 }
