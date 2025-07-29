ab29d21fd185b2c2167f13ff14fd9c83f55b527f
Pankratov <pnrf@yandex.ru>
2025-07-29 14:05:04 +0300

    docs: update content

--- src/content/portfolio/ai-services/_meta.js
+++ src/content/portfolio/ai-services/_meta.js
@@ -1,10 +1,5 @@
 export default {
 	'about-project': 'Описание проекта',
 	'intro': 'Описание сервиса',
-	'documents-list': 'Примеры документов',
-	'ai-service': {
-    type: 'separator'
-  },
-	'normative-acts': 'Нормативные акты',
-	'technical-description': 'Техническое описание'
+	'documents-list': 'Примеры документов'
 }
