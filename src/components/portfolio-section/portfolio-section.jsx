453505d8009484d0b4d2355630f752a939d48f77
Pankratov <pnrf@yandex.ru>
2025-07-31 00:17:48 +0300

    content: add blocks to main page

--- src/components/portfolio-section/portfolio-section.jsx
+++ src/components/portfolio-section/portfolio-section.jsx
@@ -45,8 +45,21 @@ export default function PortfolioSection() {
 					</div>
 				</div>
 
-				<div className={`${styles['portfolio-section__card']} ${styles['portfolio-section__card_highlight']}`}>
+				{/* <div className={`${styles['portfolio-section__card']} ${styles['portfolio-section__card_highlight']}`}>
 					<h3 className={`${styles['portfolio-section__card-title']} ${styles['portfolio-section__card-title_highlight']}`}>Movies Explorer</h3>
+					<div className={styles['portfolio-section__card-content']}>
+						<ul className={styles['portfolio-section__card-list']}>
+							<p className={styles['portfolio-section__card-list-item']}>Веб-приложение с личным кабинетом для просмотра каталога фильмов</p>
+							<br />
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link-highlight']} href='/portfolio/movies-explorer/about-project'>Описание проекта</a></li>
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link-highlight']} href='/portfolio/movies-explorer/intro'>Описание веб-приложения</a></li>
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link-highlight']} href='/portfolio/movies-explorer/api-specification'>Описание API</a></li>
+						</ul>
+					</div>
+				</div> */}
+
+				<div className={styles['portfolio-section__card']}>
+					<h3 className={styles['portfolio-section__card-title']}>Movies Explorer</h3>
 					<div className={styles['portfolio-section__card-content']}>
 						<ul className={styles['portfolio-section__card-list']}>
 							<p className={styles['portfolio-section__card-list-item']}>Веб-приложение с личным кабинетом для просмотра каталога фильмов</p>
@@ -58,6 +71,33 @@ export default function PortfolioSection() {
 					</div>
 				</div>
 
+				<div className={styles['portfolio-section__card']}>
+					<h3 className={styles['portfolio-section__card-title']}>UI-компоненты</h3>
+					<div className={styles['portfolio-section__card-content']}>
+						<ul className={styles['portfolio-section__card-list']}>
+							<p className={styles['portfolio-section__card-list-item']}>Документация по веб-компонентам в Storybook.js.org</p>
+							<br />
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link']} href='#'>Описание проекта</a></li>
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link']} href='#'>Ссылка на документацию</a></li>
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link']} href='#'>Ссылка на программный код</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div className={`${styles['portfolio-section__card']} ${styles['portfolio-section__card_highlight']}`}>
+					<h3 className={`${styles['portfolio-section__card-title']} ${styles['portfolio-section__card-title_highlight']}`}>Подборки</h3>
+					<div className={styles['portfolio-section__card-content']}>
+						<ul className={styles['portfolio-section__card-list']}>
+							<p className={styles['portfolio-section__card-list-item']}>Сборник материалов для быстрого к ним доступа и просмотра</p>
+							<br />
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link-highlight']} href='#'>Технические документы</a></li>
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link-highlight']} href='#'>Юридические документы</a></li>
+							<li className={styles['portfolio-section__card-list-item']}><a className={styles['portfolio-section__link-highlight']} href='#'>Схемы и диаграммы</a></li>
+						</ul>
+					</div>
+				</div>
+
+				
 				{/* <div className={styles['portfolio-section__card']}>
 					<h3 className={styles['portfolio-section__card-title']}>Документы</h3>
 					<div className={styles['portfolio-section__card-content']}>
